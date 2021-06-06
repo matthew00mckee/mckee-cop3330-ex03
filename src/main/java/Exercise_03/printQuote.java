79d3240a5fa77e13fa60f1c9ddbc5b985974d37d
matthew00mckee <67074056+matthew00mckee@users.noreply.github.com>
2021-06-06 13:03:59 -0400

    added comments

--- src/main/java/Exercise_03/printQuote.java
+++ src/main/java/Exercise_03/printQuote.java
@@ -1,3 +1,8 @@
+/*
+UCF cop 3330 summer 2021 assignment 1 solution
+copyright 2021 Matthew McKee
+ */
+
 package Exercise_03;
 
 import java.util.Scanner;
@@ -6,11 +11,14 @@ public class printQuote {
     public static void main(String[] args){
         System.out.print("What is the quote?");
         Scanner input = new Scanner(System.in);
+        //setup for the quote string
         String quote = input.nextLine();
 
         System.out.print("Who said it? ");
+        //setup for the person who said the quote
         String person = input.nextLine();
 
-        System.out.print(person + " says, \""+ quote + "\"" );
+        //print out for the quote with the quotation marks as escape characters
+        System.out.print(person + " says, \""+ quote +"\"" );
     }
 }
